fdcd9e5b89ce21022148394c0ff9d9cbcc1a3430
George12345max <lotzgeorge@gmail.com>
2025-09-25 15:21:10 +0200

    Task 4 Textbased menu for a game
    Task 4

---
+++ MainXD.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        // 4.f Create actions ArrayList
+        ArrayList<String> actions = new ArrayList<String>();
+        actions.add("1) Start game");
+        actions.add("2) Resume game");
+        actions.add("3) Pause game");
+        actions.add("4) End game");
+
+        // 4.g Instantiate GameMenu with actions
+        GameMenu gameMenu = new GameMenu(actions);
+
+        // 4.i Test the displayMenu() method
+        gameMenu.displayMenu();
+    }
+}
+
