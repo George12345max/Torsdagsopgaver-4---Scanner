f84925cf4cbcf2bfef25b473d33322ba64225e3d
George12345max <lotzgeorge@gmail.com>
2025-09-25 14:56:16 +0200

    Task 1
    Task 1

---
+++ Main.java
@@ -0,0 +1,3 @@
+//task 1
+//In this exercise you will write an entity class called Team and a client class called Main. You will create private attributes in the entity class and give it a parameterized constructor, a toString() and a setter method.
+
